3291beceb2b23d78119ef2103af3042becc843d3
Vasil Panov <mrvasep@gmail.com>
2024-02-03 12:21:38 +0100

    Initial Commit

---
+++ ZiveStocks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZiveStocks
+//
+//  Created by Vail Panov on 3.2.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZiveStocks/ZiveStocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZiveStocksApp.swift
+//  ZiveStocks
+//
+//  Created by Vail Panov on 3.2.24.
+//
+
+import SwiftUI
+
+@main
+struct ZiveStocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
